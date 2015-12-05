f67c82773b9fe187ccaf23fe1e3e7ac1791f5a80
petar2hypnotize <petar@2hypnotize.com>
2015-12-05 17:05:05 +0000

    Style flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
    @article = Article.new(article_params)
    
    if @article.save
-     flash[:notice] = "Article was sucessfully created"
+     flash[:success] = "Article was sucessfully created"
      redirect_to article_path(@article)
    else
      render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update 
     
     if @article.update(article_params)
-      flash[:notice] = "Articla was successfully updated"
+      flash[:success] = "Articla was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Article was sucessfully deleted"
+    flash[:danger] = "Article was sucessfully deleted"
     redirect_to articles_path
   end
   
